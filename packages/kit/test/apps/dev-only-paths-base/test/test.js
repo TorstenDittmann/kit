9f12837d72dba8ecd023c3a2c8c043ce0ae9c017
Torsten Dittmann <torsten.dittmann@googlemail.com>
2024-08-14 20:47:48 +0200

    chore: run formatter

--- packages/kit/test/apps/dev-only-paths-base/test/test.js
+++ packages/kit/test/apps/dev-only-paths-base/test/test.js
@@ -8,13 +8,13 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 /** @typedef {import('@playwright/test').Response} Response */
 
-const BASE_PATH = config.kit.paths.base;
+const base_path = config.kit.paths.base;
 
 test('service worker import proxy', async ({ baseURL }) => {
-	const service_worker = await fetch(baseURL + BASE_PATH + '/service-worker.js').then((r) =>
+	const service_worker = await fetch(baseURL + base_path + '/service-worker.js').then((r) =>
 		r.text()
 	);
 	expect(service_worker).toEqual(
-		`import '${path.join(BASE_PATH, '/@fs', __dirname, '../src/service-worker.js')}';`
+		`import '${path.join(base_path, '/@fs', __dirname, '../src/service-worker.js')}';`
 	);
 });
